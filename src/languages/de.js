fc179a2055cecc72687008a3cdef77abb5dfaab9
knhakan <knhakan@gmail.com>
2021-11-08 18:12:31 +0100

    language files added to handle translation

---
+++ src/languages/de.js
@@ -0,0 +1,8 @@
+export default {
+    welcome: "Willkommen!",
+    cardTitle: "Kartentitel",
+    cardOneDescription: "Dies ist eine breitere Karte mit unterstützendem Text unten als natürlicher Einstieg in zusätzliche Inhalte. Dieser Inhalt ist etwas länger.",
+    cardTwoDescription: "Diese Karte enthält unten den unterstützenden Text als natürlichen Einstieg in zusätzliche Inhalte.",
+    lastUpdate: "Letzte Aktualisierung",
+    minutes: "Protokoll"
+}
---
+++ src/languages/en.js
@@ -0,0 +1,8 @@
+export default {
+    welcome: "Welcome!",
+    cardTitle: "Card title",
+    cardOneDescription: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+    cardTwoDescription: "This card has supporting text below as a natural lead-in to additional content.",
+    lastUpdate: "Last updated",
+    minutes: "minutes"
+}
